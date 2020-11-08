5c653eaa26b0f6497b2400e394300a1b1214e1a3
Stilyan Dimov <stilyan.r.dimov@gmail.com>
2020-11-08 19:21:41 +0200

    Testing FirstController & View

---
+++ Web/MyWebtoonWebProject.Web/Controllers/WebtoonsController.cs
@@ -0,0 +1,12 @@
+﻿namespace MyWebtoonWebProject.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class WebtoonsController : BaseController
+    {
+        public IActionResult CreateWebtoon()
+        {
+            return this.View();
+        }
+    }
+}
