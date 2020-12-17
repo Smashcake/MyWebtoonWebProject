ccbc604df898a654f0c8e97c20a7b01b9bddb0e4
Stilyan Dimov <stilyan.r.dimov@gmail.com>
2020-12-17 22:36:59 +0200

    Got 100% coverage for CommentsVotesServiceTests

--- MyWebtoonWebProject.Services.Data.Tests/CommentsVotesServiceTests.cs
+++ MyWebtoonWebProject.Services.Data.Tests/CommentsVotesServiceTests.cs
@@ -31,25 +31,28 @@
                 ApplicationUserId = "pesho",
                 Vote = MyWebtoonWebProject.Data.Models.Enums.VoteType.UpVote
             };
+            var secondCommentVote = new CommentVote()
+            {
+                CommentId = "test",
+                ApplicationUserId = "ivan",
+                Vote = MyWebtoonWebProject.Data.Models.Enums.VoteType.DownVote
+            };
             commentVotes.Add(commentVote);
             comment.CommentVotes.Add(commentVote);
+            comment.CommentVotes.Add(secondCommentVote);
+
 
             var mockCommentsRepo = new Mock<ICommentsRepository>();
             mockCommentsRepo.Setup(x => x.All()).Returns(comments.AsQueryable());
             var mockCommentVotesRepo = new Mock<ICommentsVotesRepository>();
-            mockCommentVotesRepo.Setup(x => x.GetCommentVoteByIds(comment.Id, comment.CommentAuthorId)).Returns(commentVote);
+            mockCommentVotesRepo.Setup(x => x.GetCommentVotesByCommentId("test")).Returns(comment.CommentVotes);
 
             var service = new CommentsVotesService(mockCommentsRepo.Object, mockCommentVotesRepo.Object);
 
-            //TESTED IN DEBUG EVERYTHING IS FOUND UNTIL LIKES 
-            //BUT EF BREAKS AND CANNOT FINISH THE SUM EQUATION
-            //WORKS ON LIVE SERVER
-
-            Assert.True(true);
-            //var vote = service.GetCommentVotes("test123");
-            //Assert.Equal("test123", vote.CommentNumber);
-            //Assert.Equal(0, vote.Dislikes);
-            //Assert.Equal(1, vote.Likes);
+            var vote = service.GetCommentVotes("test123");
+            Assert.Equal("test123", vote.CommentNumber);
+            Assert.Equal(1, vote.Dislikes);
+            Assert.Equal(1, vote.Likes);
         }
 
         [Fact]
@@ -132,6 +135,14 @@
             Assert.Equal("UpVote", goshosVote.Vote.ToString());
             Assert.Equal("gosho", goshosVote.ApplicationUserId);
             Assert.Equal("test", goshosVote.CommentId);
+
+            await service.UserCommentVoteAsync("test123", false, "ivan");
+
+            var ivansVote = commentVotes.ToArray()[2];
+            Assert.Equal(3, commentVotes.Count);
+            Assert.Equal("DownVote", ivansVote.Vote.ToString());
+            Assert.Equal("ivan", ivansVote.ApplicationUserId);
+            Assert.Equal("test", ivansVote.CommentId);
         }
     }
 }
